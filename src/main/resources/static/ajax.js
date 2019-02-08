c2f7cc211d56bd0714c9b41135252b16898ba0a9
J Shim <shimjay1@gmail.com>
2019-02-08 11:10:13 -0500

    slowly losing my mind with this refresh bug...

--- src/main/resources/static/ajax.js
+++ src/main/resources/static/ajax.js
@@ -81,71 +81,22 @@ function displayPatients(xhr){
     }
 }
 
-// 1) Remove all rows from doctor table, 2) grab all doctors from database and add to table.
-function grabDoctors(){
-	removeTableRows("dtable");
-	sendAjaxGet(doctorData, displayDoctors);
-}
-
-// 1 more step to grab top heartRate for all doctors: addDoctorTop()
-function displayDoctors(xhr){
-	let data = JSON.parse(xhr.response);
-	sendAjaxGet2(doctorTop, addDoctorTop, data);
-}
-
-// Adds the highest heartRate of each doctor to themselves.
-// Use RegEx to parse through JSON response data for highest heartRate.
-function addDoctorTop(xhr, allDoc){
-	let data = JSON.parse(xhr.response);
-	// Find first digit in string and any digits directly following after.
-	var myRe = /^[^\d]*(\d+)/;
-	
-	for(j = 0; j < allDoc.length; j ++){
-		for(i = 0; i < Object.size(data); i++){
-			var idOfDoc = myRe.exec(Object.entries(data)[i][0])[1];
-			
-			if(idOfDoc == allDoc[j].id){
-				addDoctor(allDoc[j].id, allDoc[j].firstName, allDoc[j].lastName, Object.entries(data)[i][1]);
-			}
-		}
-	}
-}
-
-// Add doctor to table.
-function addDoctor(id, first, last, high){
-	let row = document.createElement("tr");
-	let cell0 = document.createElement("td");
-    let cell1 = document.createElement("td");
-    let cell2 = document.createElement("td");
-    let cell3 = document.createElement("td");
-    
-    row.appendChild(cell0);
-    row.appendChild(cell1);
-    row.appendChild(cell2);
-    row.appendChild(cell3);
-	
-	cell0.innerHTML = id;
-	cell1.innerHTML = first;
-	cell2.innerHTML = last;
-	cell3.innerHTML = high;
-	
-	document.getElementById("dbody").appendChild(row);
-}
-
 // Invoked when patient update button is clicked and user id does not exist.
 function createPatient(){
 	// getDoctorBy: Id
-	getDoctorByX(createNewPatient, document.getElementById("patient-update-doctor").value);
+	doctorId = document.getElementsByClassName("form-group")[0][4].value;
+	getDoctorByX(createNewPatient, doctorId);
 }
 
 // After getDoctorById, add that doctor to our newPatient Object.
 // POST newPatient to database.
 function createNewPatient(xhr, info){
 	let data = JSON.parse(xhr.response);
-	let id = document.getElementById("patient-update-id").value;
-	let first = document.getElementById("patient-update-first").value;
-	let last = document.getElementById("patient-update-last").value;
-	let heart = document.getElementById("patient-update-heart").value;
+	let form = document.getElementsByClassName("form-group")[0];
+	let id = form[0].value;
+	let first = form[1].value;
+	let last = form[2].value;
+	let heart = form[3].value;
 	let doctor;
 	for(i = 0; i < data.length; i++){
 		if(data[i].id == info){
@@ -160,6 +111,7 @@ function createNewPatient(xhr, info){
 		"heartRate": heart,
 		"doctor": doctor
 	}
+	console.log("Patient " + newPatient.firstName + " has been added.");
 	
 	// After POST-ing newPatient, add newPatient to the patient table.
 	sendAjaxPost(patient, addNewPatient, newPatient);
@@ -172,7 +124,6 @@ function addNewPatient(newPatient){
 	let last = newPatient.lastName;
 	let doctorId = newPatient.doctor.id;
 	addPatient(id, first, last, heart, doctorId);
-	pageRefresh();
 }
 
 // Add patients directly to table.
@@ -242,7 +193,7 @@ function putPatient(xhr, uPatient){
 			}
 		}
 	}
-	sendAjaxPut(patient, pageRefresh, uPatient);
+	sendAjaxPut(patient, doNothing, uPatient);
 }
 
 // Invoked when deletePatient button is pressed.
@@ -260,12 +211,64 @@ function deletePatientInfo(xhr, pid){
 			delPatient = data[i];
 		}
 	}
-	sendAjaxDelete(patient, pageRefresh, delPatient);
+	sendAjaxDelete(patient, doNothing, delPatient);
+}
+
+//1) Remove all rows from doctor table, 2) grab all doctors from database and add to table.
+function grabDoctors(){
+	removeTableRows("dtable");
+	sendAjaxGet(doctorData, displayDoctors);
+}
+
+// 1 more step to grab top heartRate for all doctors: addDoctorTop()
+function displayDoctors(xhr){
+	let data = JSON.parse(xhr.response);
+	sendAjaxGet2(doctorTop, addDoctorTop, data);
+}
+
+// Adds the highest heartRate of each doctor to themselves.
+// Use RegEx to parse through JSON response data for highest heartRate.
+function addDoctorTop(xhr, allDoc){
+	let data = JSON.parse(xhr.response);
+	// Find first digit in string and any digits directly following after.
+	var myRe = /^[^\d]*(\d+)/;
+	
+	for(j = 0; j < allDoc.length; j ++){
+		for(i = 0; i < Object.size(data); i++){
+			var idOfDoc = myRe.exec(Object.entries(data)[i][0])[1];
+			
+			if(idOfDoc == allDoc[j].id){
+				addDoctor(allDoc[j].id, allDoc[j].firstName, allDoc[j].lastName, Object.entries(data)[i][1]);
+			}
+		}
+	}
+}
+
+// Add doctor to table.
+function addDoctor(id, first, last, high){
+	let row = document.createElement("tr");
+	let cell0 = document.createElement("td");
+    let cell1 = document.createElement("td");
+    let cell2 = document.createElement("td");
+    let cell3 = document.createElement("td");
+    
+    row.appendChild(cell0);
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+	
+	cell0.innerHTML = id;
+	cell1.innerHTML = first;
+	cell2.innerHTML = last;
+	cell3.innerHTML = high;
+	
+	document.getElementById("dbody").appendChild(row);
 }
 
 // HELPER FUNCTIONS:
 // Object.size: returns size of Object.
 // removeTableRows: removes all rows of table.
+// doNothing: literally do nothing.
 Object.size = function(obj) {
     var size = 0, key;
     for (key in obj) {
@@ -287,6 +290,10 @@ function removeTableRows(tableType){
 	for (x = tableRows.length - 1; x > 0; x--) {
 	   table.removeChild(tableRows[x]);
 	}
+}
+
+function doNothing(){
+	
 }
 
 // AJAX GET call, without information.
